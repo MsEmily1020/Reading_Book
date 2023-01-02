8d1a87a915f87ae391211c8a458000cbeb26a7b0
MsEmily1020 <peace10200khs@gmail.com>
2023-01-02 10:52:02 +0900

    2st class : 'create method - full product'

--- src/product/controller/ProductController.java
+++ src/product/controller/ProductController.java
@@ -12,14 +12,30 @@ public class ProductController extends JFrame {
 	ProductDAO dao = new ProductDAO();
 	ArrayList<ProductVO> pvo = dao.select();
 	ProductView view = new ProductView();
-	
+
 	public ProductController() {
-		add(view, "Center");
 		setTitle("음료수자판기");
 		setSize(500, 600);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(2);
 		setResizable(false);
+	}
+
+	//제품 채우기
+	public void fullProduct() {
+		String[] menu = "초록매실,콜라,사이다,모구모구,생수,알로에,레몬에이드".split(",");
+		int[] price = { 1500, 1400, 1400, 1500, 800, 1200, 1100 };
+		ProductVO vo = null;
+		pvo = dao.select();
+		for (int i = 0; i < menu.length; i++) {
+			vo = new ProductVO();
+			vo.setName(menu[i]);
+			vo.setPrice(price[i]);
+			vo.setProductNum(i);
+			vo.setStockNum(10);
+			vo.setImageName(menu[i]);
+			pvo.add(vo);
+		}
 	}
 	
 	public static void main(String[] args) {
--- src/product/view/ProductView.java
+++ src/product/view/ProductView.java
@@ -3,6 +3,7 @@ package product.view;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -10,19 +11,23 @@ import product.vo.ProductVO;
 
 public class ProductView extends JPanel {
 	ArrayList<ProductVO> pvo;
-	ArrayList<JLabel> jList = new ArrayList<JLabel>();
+	ArrayList<JLabel> lbList = new ArrayList<JLabel>();
 	
 	public JPanel displayProducts() {
 		JPanel gnPn = new JPanel(new GridLayout(3, 3));
-		
+		for (ProductVO vo : pvo) {
+			ImageIcon icon = new ImageIcon("images/" + vo.getImageName() + ".jpg");
+			JLabel lb = new JLabel(icon);
+			lbList.add(lb);
+			gnPn.add(lb);
+		}
 		return gnPn;
 	}
 	
 	public void inputPurchase() {
-		
 	}
 	
 	public void setProductList(ArrayList<ProductVO> pvo) {
-		
+		this.pvo = pvo;
 	}
 }
