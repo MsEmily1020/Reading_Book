998b36e0592b40a1aa3999fa3f8bd948b0c9dd91
MsEmily1020 <peace10200khs@gmail.com>
2023-01-02 11:31:36 +0900

    3st class : 'view(connect) the frame'

--- src/product/controller/ProductController.java
+++ src/product/controller/ProductController.java
@@ -3,17 +3,24 @@ package product.controller;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import product.view.ProductView;
 import product.vo.ProductDAO;
 import product.vo.ProductVO;
 
 public class ProductController extends JFrame {
-	ProductDAO dao = new ProductDAO();
-	ArrayList<ProductVO> pvo = dao.select();
-	ProductView view = new ProductView();
+	public static final int STOP = 0;
 
+	ArrayList<ProductVO> pvo;
+	
 	public ProductController() {
+		ProductView view = new ProductView();
+		fullProduct();
+		view.setProductList(pvo);
+		JPanel panC = view.displayProducts();
+		
+		add(panC, "Center");
 		setTitle("음료수자판기");
 		setSize(500, 600);
 		setLocationRelativeTo(null);
@@ -23,8 +30,9 @@ public class ProductController extends JFrame {
 
 	//제품 채우기
 	public void fullProduct() {
-		String[] menu = "초록매실,콜라,사이다,모구모구,생수,알로에,레몬에이드".split(",");
-		int[] price = { 1500, 1400, 1400, 1500, 800, 1200, 1100 };
+		ProductDAO dao = new ProductDAO();
+		String[] menu = "초록매실,콜라,사이다,모구모구,생수,알로에,레몬에이드,웰치스,포카리스웨트".split(",");
+		int[] price = { 1500, 1400, 1400, 1500, 800, 1200, 1100, 1200, 1300 };
 		ProductVO vo = null;
 		pvo = dao.select();
 		for (int i = 0; i < menu.length; i++) {
