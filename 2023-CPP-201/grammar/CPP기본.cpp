0b39b1143b995e2333a9b9520fe5c2d47ea40a70
MsEmily1020 <peace10200khs@gmail.com>
2023-03-16 10:15:09 +0900

    add : namespace

--- 2023-CPP-201/grammar/CPP기본.cpp
+++ 2023-CPP-201/grammar/CPP기본.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
+using namespace std;	// 이름 공간 (std:: 생략, 변수 등 중복 방지)
+
 int main() {
 	// std::cout : 출력을 위한 객체
 	// << : 출력연산자
 	// std::endl : 개형(줄바꿈) 객체
-	std::cout << "Hello World!" << std:: endl;
+	cout << "Hello World!" << endl;
 	return 0;
 }
