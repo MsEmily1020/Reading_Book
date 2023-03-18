303799925652b129a6c22dbe7d34f958b86e9802
MsEmily1020 <peace10200khs@gmail.com>
2023-03-18 18:24:16 +0900

    change hores : 5, betting-person : 2

--- src/racehores/BettingPerson.java
+++ src/racehores/BettingPerson.java
@@ -1,23 +1,31 @@
 package racehores;
 
 public class BettingPerson {
-	String name;
-	int betting;
-	
+	private int orderNum;
+	private String name;
+	private int betingIndex;
+
+	public int getBetingIndex() {
+		return betingIndex;
+	}
+
+	public void setBetingIndex(int betingIndex) {
+		this.betingIndex = betingIndex;
+	}
+
+	public int getOrderNum() {
+		return orderNum;
+	}
+
+	public void setOrderNum(int orderNum) {
+		this.orderNum = orderNum;
+	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public int getBetting() {
-		return betting;
-	}
-	
-	public void setBetting(int betting) {
-		this.betting = betting;
-	}
-	
 }
--- src/racehores/RaceHorse.java
+++ src/racehores/RaceHorse.java
@@ -1,9 +1,7 @@
 package racehores;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.ImageIcon;
@@ -13,123 +11,144 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class RaceHorse extends JFrame {
-	JLabel[] horses = new JLabel[3];
+	JLabel[] horses = new JLabel[5];
 	HorseThread[] hts = new HorseThread[horses.length];
-	int[] winnerIndex = new int[horses.length];
+	int winnerIndex[] = new int[horses.length];
 	int index;
-	String[] comboStr = "1번: horse,2번: human,3번: bird".split(",");
+	String comboStr[] = { "1번: 귀염둥이 제리", "2번: 잠좀자자 자라", "3번: 맛있는 사슴 ", "4번: 꼬끼욧 늑대", "5번: 노랑노랑 새" };
 	JComboBox<String> combo = new JComboBox<String>(comboStr);
-	ArrayList<BettingPerson> btList = new ArrayList<BettingPerson>();
-	JTextField tf = new JTextField(10);
 	int betingIndex;
+	BettingPerson bet1, bet2;
 
-	public RaceHorse() {		
+	public RaceHorse() {
 		JPanel pan = new JPanel(null);
-		pan.setBackground(Color.white);
-		JPanel panN = new JPanel();
-		panN.setBackground(Color.white);
-		
-		JLabel lineLbl = new JLabel(new ImageIcon("image/line.png"));
-		lineLbl.setBounds(540, 27, 5, 420);
-		JLabel flagLbl = new JLabel(new ImageIcon("image/flag.png"));
-		flagLbl.setBounds(530, 5, 20, 27);
+		ImageIcon icon = null;
+		JLabel lineLbl = new JLabel(new ImageIcon("imgs/line.png"));
+		lineLbl.setBounds(540, 10, 5, 500);
+		JLabel flagLbl = new JLabel(new ImageIcon("imgs/flag.png"));
+		flagLbl.setBounds(540, 5, 20, 27);
 		pan.add(lineLbl);
 		pan.add(flagLbl);
-		
-		JButton btnBeting = new JButton("게임배팅");
+
+		JPanel panN = new JPanel();
+
+		JButton btnBeting1 = new JButton("게임배팅1");
+		JButton btnBeting2 = new JButton("게임배팅2");
 		JButton btnStart = new JButton("게임시작");
-		btnBeting.addActionListener(btnL);
+		btnBeting1.addActionListener(btnL);
+		btnBeting2.addActionListener(btnL);
 		btnStart.addActionListener(btnL);
-		panN.add(tf);
 		panN.add(combo);
-		panN.add(btnBeting);
+		panN.add(btnBeting1);
+		panN.add(btnBeting2);
 		panN.add(btnStart);
-		
+
 		for (int i = 0; i < horses.length; i++) {
-			horses[i] = new JLabel(new ImageIcon("image/img" + (i+1) + ".gif"));
-			horses[i].setLocation(0, 50 + i * 120);
-			horses[i].setSize(100, 100);
+			icon = new ImageIcon("imgs/horse" + (i + 1) + ".gif");
+			horses[i] = new JLabel(icon);
+			horses[i].setLocation(0, 50 + i * 85);
+			horses[i].setSize(60, 40);
 			pan.add(horses[i]);
 		}
-		
+
 		add(pan, "Center");
 		add(panN, "North");
-		setTitle("경주마게임");
-		setDefaultCloseOperation(2);
-		setSize(600, 500);
+		setTitle("경주마 게임");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(610, 520);
 		setLocationRelativeTo(null);
-		setResizable(false);		
+		setVisible(true);
+		setResizable(false);
+
+		/*
+		 * for (int i = 0; i < horses.length; i++) { hts[i] = new HorseThread(horses[i],
+		 * "stop_horse" + (i + 1), i); hts[i].start(); }
+		 */
+
 	}
 
 	public static void main(String[] args) {
-		new RaceHorse().setVisible(true);
+		new RaceHorse();
+
 	}
-	
+
 	ActionListener btnL = new ActionListener() {
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
 			switch (e.getActionCommand()) {
-			case "게임배팅":
-				BettingPerson btPs = new BettingPerson();
-				btPs.setName(tf.getText());
-				btPs.setBetting(combo.getSelectedIndex());
-				btList.add(btPs);
+			case "게임배팅1":
+				bet1 = new BettingPerson();
+				bet1.setOrderNum(1);
+				bet1.setName(JOptionPane.showInputDialog("배팅하는 사람의 이름을 입력하세요"));
+				bet1.setBetingIndex(combo.getSelectedIndex());
 				break;
-			case "게임시작":
+			case "게임배팅2":
+				bet2 = new BettingPerson();
+				bet2.setOrderNum(2);
+				bet2.setName(JOptionPane.showInputDialog("배팅하는 사람의 이름을 입력하세요"));
+				bet2.setBetingIndex(combo.getSelectedIndex());
+				break;
+			case "게임시작": // 게임시작을 클릭했을 시
 				for (int i = 0; i < horses.length; i++) {
-					HorseThread t = new HorseThread(horses[i], i);
+					HorseThread t = new HorseThread(horses[i], "stop_horse" + (i + 1), i);
 					t.start();
 				}
 				break;
 			}
+
 		}
 	};
 
-	public class HorseThread extends Thread{
+	public class HorseThread extends Thread {
 		JLabel lblHorse;
+		String stopImageName;
 		int randomValue;
 		int horseIndex;
-		
-		public HorseThread(JLabel lblHorse, int horseIndex) {
+
+		public HorseThread(JLabel lblHorse, String stopImageName, int horseIndex) {
 			this.lblHorse = lblHorse;
+			this.stopImageName = stopImageName;
 			this.horseIndex = horseIndex;
 		}
 
 		@Override
 		public void run() {
 			while (true) {
-				lblHorse.setLocation(lblHorse.getX()+5, lblHorse.getY());
-				if(lblHorse.getX()>=540) {
+				lblHorse.setLocation(lblHorse.getX() + 5, lblHorse.getY());
+				if (lblHorse.getX() == 540) {
+					lblHorse.setIcon(new ImageIcon("images/" + stopImageName + ".gif"));
 					winnerIndex[index++] = horseIndex;
-					if(index == horses.length-1) {
-						JOptionPane.showMessageDialog(RaceHorse.this, (winnerIndex[0]+1)+"말이 우승!!!");
-						if(winnerIndex[0]==btList.get(0).getBetting() && winnerIndex[0] != btList.get(1).getBetting())
-							JOptionPane.showMessageDialog(RaceHorse.this, "축하합니다. " + btList.get(0).getName() + "님이 배팅에 성공하였습니다.");
-						else if(winnerIndex[0] == btList.get(0).getBetting())
-							JOptionPane.showMessageDialog(RaceHorse.this, "두 분다 배팅에 성공하셨군요.");
-						else if(winnerIndex[0] == btList.get(1).getBetting() && winnerIndex[0] != btList.get(0).getBetting())
-							JOptionPane.showMessageDialog(RaceHorse.this, "축하합니다. " + btList.get(1).getName() + "님이 배팅에 성공하였습니다.");
-						else	
-							JOptionPane.showMessageDialog(RaceHorse.this, "두 분다 배팅에 성공 못하셨어요 ㅠㅠ");
+					if (index == horses.length - 1) {
+						JOptionPane.showMessageDialog(RaceHorse.this, (winnerIndex[0] + 1) + "번째 말 우승!!!");
+						if (winnerIndex[0] == bet1.getBetingIndex())
+							JOptionPane.showMessageDialog(RaceHorse.this,
+									"축하합니다. " + bet1.getName() + "님 배팅에 성공하였습니다!!");
+						else if (winnerIndex[0] == bet2.getBetingIndex())
+							JOptionPane.showMessageDialog(RaceHorse.this,
+									"축하합니다. " + bet2.getName() + "님 배팅에 성공하였습니다!!");
+						else
+							JOptionPane.showMessageDialog(RaceHorse.this, bet1.getName() + "님, " + bet2.getName() + "님 모두 배팅에 실패하였습니다. 다시 도전하실래요?");
 						index = 0;
-						for (int i = 0; i < horses.length; i++)
+						for (int i = 0; i < horses.length; i++) {
+//							System.out.println(winnerIndex[i]);
 							horses[i].setLocation(0, horses[i].getY());
-						btList = new ArrayList<BettingPerson>();
+							horses[i].setIcon(new ImageIcon("imgs/horse" + (i + 1) + ".gif"));
+						}
+
 					}
 					break;
 				}
-				
 				try {
 					Random random = new Random();
-					sleep(10 * random.nextInt(10));
+					randomValue = random.nextInt(10); // 0~9 사이의 값
+					sleep(10 * randomValue);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-			}			
+			}
 		}
 	}
 }
