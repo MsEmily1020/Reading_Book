8cb2e7874b0260d92d5789fdfc5babcca87aeb5b
MsEmily1020 <peace10200khs@gmail.com>
2023-03-23 10:02:54 +0900

    before style.c : 문자열 복사

---
+++ 2023-CPP-201/grammar/문자열복사.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h> // 문자열 관련 함수
+
+using namespace std;
+
+int main() {
+	char str[30] = { 0 };
+	char str2[30] = "c_insert";
+
+	for (int i = 0; str2[i] != '\0'; i++) {
+		str[i] = str2[i];
+	}
+
+	printf("%s \n", str);
+	return 0;
+}
