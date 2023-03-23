7c340cbe989646b9d4f368390882ab209fee9fde
MsEmily1020 <peace10200khs@gmail.com>
2023-03-23 10:19:08 +0900

    after style.c++ : string 복사

--- 2023-CPP-201/grammar/문자열복사.cpp
+++ 2023-CPP-201/grammar/문자열복사.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <string.h> // 문자열 관련 함수
+#include <string> // std::string 들어있음(.h x)
 
 using namespace std;
 
 int main() {
-	char str[30] = { 0 };
-	char str2[30] = "c_insert";
+	string str1;
+	string str2 = "c_insert";
 
-	strcpy(str, str2); //str에 str2에 있는 문자열 복사 집어넣음
+	str1 = str2;
 
-	printf("%s \n", str);
+	cout << str1 << endl;
 	return 0;
 }
