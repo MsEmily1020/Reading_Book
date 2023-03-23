890b85a35a04b6a5fca028b0e4a02144a8f7a159
MsEmily1020 <peace10200khs@gmail.com>
2023-03-23 11:03:11 +0900

    before style.c : strcat()

--- 2023-CPP-201/grammar/string더하기.cpp
+++ 2023-CPP-201/grammar/string더하기.cpp
@@ -10,12 +10,8 @@ int main() {
 	char str1[30] = "Jiwoo";
 	char str2[30] = "Rock";
 
-	const int str1_len = strlen(str1);
-	const int str2_len = strlen(str2);
-
-	for (int i = str1_len; i < str1_len + str2_len; i++) {
-		str1[i] = str2[i - str1_len];
-	}
-
+	strcat(str1, str2);
 	printf("%s\n", str1);
+
+	return 0;
 }
