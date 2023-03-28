208094a165038bb16636d64d098bf62ecb976173
MsEmily1020 <peace10200khs@gmail.com>
2023-03-28 16:48:22 +0900

    count함수

--- CosPro_Python.py
+++ CosPro_Python.py
@@ -1,4 +1,6 @@
 #sum
+
+# Python 방식
 def sum_list1(scores):
     sum_ = 0
     for score in scores:
@@ -7,8 +9,48 @@ def sum_list1(scores):
 
 print(sum_list1([3, 28, 4, 34, 2023]))
 
+
+# C, Java 방식
+def sum_list2(scores):
+    sum_ = 0
+    for i in range(len(scores)):
+        sum_ += scores[i]
+    return  sum_
+
+print(sum_list2([3, 28, 4, 34, 2023]))
+
+def sum_list3(scores):
+    return sum(scores)
+
+print(sum_list3([3, 28, 4, 34, 2023]))
+print(sum([3, 28, 4, 34, 2023]))
+
 #count
 
+# Python 방식
+def count_list1(scores):
+    count_ = 0
+    for score in scores:
+        count_ += 1
+    return count_
+
+print(count_list1([3, 28, 4, 34, 2023]))
+
+
+# C, Java 방식
+def count_list2(scores):
+    count_ = 0
+    for i in range(len(scores)):
+        count_ += 1
+    return  count_
+
+print(count_list2([3, 28, 4, 34, 2023]))
+
+def count_list3(scores):
+    return len(scores)
+
+print(count_list3([3, 28, 4, 34, 2023]))
+print(len([3, 28, 4, 34, 2023]))
 #max
 
 #min
