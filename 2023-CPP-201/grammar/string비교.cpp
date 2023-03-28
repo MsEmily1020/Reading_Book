1ca838637a7095af1d3d845d41717d940f899f4e
MsEmily1020 <peace10200khs@gmail.com>
2023-03-28 09:11:27 +0900

    before style.c - strcmp

--- 2023-CPP-201/grammar/string비교.cpp
+++ 2023-CPP-201/grammar/string비교.cpp
@@ -10,37 +10,16 @@ int main() {
 	char str1[50] = "Hello";
 	char str2[50] = "World";
 
-	const int s1_len = strlen(str1);
-	const int s2_len = strlen(str2);
-
-	// s1_len과 s2_len 중 작은 값을 선택
-	const int len = s1_len < s2_len ? s1_len : s2_len;
-
-	int i;
-	for (i = 0; i < len; i++) {
-		if (str2[i] > str1[i]) {
-			printf("str2 > str1 \n");
-			break;
-		}
-
-		else if (str1[i] > str2[i]) {
-			printf("str1 > str2 \n");
-			break;
-		}
+	// 함수 인자 위치와 결과값(-1, 1) 헷갈림
+	int result = strcmp(str1, str2);
+	if (result == -1) {
+		printf("str2 > str1");
 	}
-
-	if (i == len) {
-		if (s2_len > s1_len) {
-			printf("str2 > str1 \n");
-		}
-
-		else if (s1_len > s2_len) {
-			printf("str1 > str2 \n");
-		}
-
-		else {
-			printf("str1 == str2");
-		}
+	else if (result == 1) {
+		printf("str2 < str1");
+	}
+	else if (result == 0) {
+		printf("str1 == str2");
 	}
 	return 0;
 }
