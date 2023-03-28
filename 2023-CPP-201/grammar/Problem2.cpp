a87c48e31d69c656e246ba8b37479171bccda2ac
MsEmily1020 <peace10200khs@gmail.com>
2023-03-28 09:55:09 +0900

    지금까지 배운 내용 문제 풀기

---
+++ 2023-CPP-201/grammar/Problem2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str = "Hello, world!";
+    int len = str.size();
+    cout << "string±æÀÌ: " << len << endl;
+	return 0;
+}
