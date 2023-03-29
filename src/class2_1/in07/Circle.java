6462e8a884e04e6191816297a738f56936aab68e
MsEmily1020 <peace10200khs@gmail.com>
2023-03-29 10:56:57 +0900

    MethodOverridingEx

---
+++ src/class2_1/in07/Circle.java
@@ -0,0 +1,7 @@
+package class2_1.in07;
+
+public class Circle extends Shape{
+	public void draw() {
+		System.out.println("원모양"); 
+	}
+}
---
+++ src/class2_1/in07/Line.java
@@ -0,0 +1,7 @@
+package class2_1.in07;
+
+public class Line extends Shape{
+	public void draw() {
+		System.out.println("라인");
+	}
+}
