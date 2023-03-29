b5e342e4b6361f4f660c8e1a89095b7f36fdc1e8
MsEmily1020 <peace10200khs@gmail.com>
2023-03-29 16:58:19 +0900

    [CosPro2급] 알파벳 바꾸기

--- CosPro_Python.py
+++ CosPro_Python.py
@@ -88,3 +88,15 @@ def make_list(n):
 print(make_list(6))
 zero_list = [0 for i in range(6)]
 print(zero_list)
+
+def solution(s):
+    s_lst = list(s)
+    n = len(s)
+    for i in range(n):
+        if s_lst[i] == 'a':
+            s_lst[i] = 'z'
+        elif s_lst[i] == 'z':
+            s_lst[i] =  'a'
+    return "".join(s_lst)           #리스트 -> 문자열로 바꿈
+
+print(solution("abz"))
