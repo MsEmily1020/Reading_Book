6907cd98adf71a29b5d29a8c2702e699485eefcd
MsEmily1020 <peace10200khs@gmail.com>
2023-03-30 09:00:59 +0900

    interface 예제 (interface 타입, new 클래스)

---
+++ src/class2_1/in12/Sound.java
@@ -0,0 +1,6 @@
+package class2_1.in12;
+
+public interface Sound {
+	public void SoundUp(int level);
+	public void SoundDown(int level);
+}
---
+++ src/class2_1/in12/TV.java
@@ -0,0 +1,23 @@
+package class2_1.in12;
+
+public class TV implements Sound {
+	private int soundLevel;
+
+	public TV() {
+		soundLevel = 0;
+	}
+
+	@Override
+	public void SoundUp(int level) {
+		soundLevel += level; 
+		System.out.println("TV볼륨 " + level + " 증가");
+	}
+
+	@Override
+	public void SoundDown(int level) {
+		soundLevel -= level;
+		if(soundLevel < 0)
+			soundLevel = 0;
+		System.out.println("TV볼륨 " + level + " 감소");
+	}
+}
