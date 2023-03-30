28604ab5c37bbcfbdb280b55a349a0f20d5f06a5
MsEmily1020 <peace10200khs@gmail.com>
2023-03-30 10:52:30 +0900

    접근제어자 class default값 -> public

--- 2023-CPP-201/grammar/클래스기본.cpp
+++ 2023-CPP-201/grammar/클래스기본.cpp
@@ -6,10 +6,13 @@ using namespace std;
 // 여러 data들을 하나의 의미있는 구조체로 묶어서 관리
 
 // 성능때문에 id, sex 문자열x -> 문자열은 정수데이터보다 상대적으로 많은 메모리 공간 요구
-// 정수 : 비교연산 한번에 가능, 문자열은 글자수만큼 반복하여 비교
-// 가독성과 유지보수를 위해 열거형(enum) 추천
-struct Student {
+// 구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로
+// class default 값 : private / struct default 값 : public 
+class Student {
+public:
 	string name;
+	// 정수 : 비교연산 한번에 가능, 문자열은 글자수만큼 반복하여 비교
+	// 가독성과 유지보수를 위해 열거형(enum) 추천
 	int id;
 	int age;
 	// 0 : 남자, 1 : 여자
