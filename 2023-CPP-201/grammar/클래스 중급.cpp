f2d90c65c6ae08489722029a7fd7a63877b0a752
MsEmily1020 <peace10200khs@gmail.com>
2023-04-18 09:43:27 +0900

    [C++] const 포인트 매개변수1

---
+++ 2023-CPP-201/grammar/클래스 중급.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class MString {
+public:
+	// 생성자
+	MString(const char* str) {
+		c_str_ = str;
+	}
+
+	// getter
+	unsigned int size() { return size_; }
+	const char* c_str() { return c_str_; }
+
+private:
+	unsigned int size_;		// 문자열의 길이
+	const char* c_str_;			// 문자열을 가리키는 주소
+};
+
+int main(void)
+{
+	MString str = MString("yakitori tabetai");
+	cout << str.c_str() << endl;
+	return 0;
+}
