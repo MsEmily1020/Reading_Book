86c9ce19f9c56dea8bb0608c9ebcae6840240b80
MsEmily1020 <peace10200khs@gmail.com>
2023-04-18 10:06:15 +0900

    [C++] 포인터 멤버변수2 (문자열 동적할당)

--- 2023-CPP-201/grammar/클래스 중급.cpp
+++ 2023-CPP-201/grammar/클래스 중급.cpp
@@ -5,17 +5,21 @@ using namespace std;
 class MString {
 public:
 	// 생성자
-	MString(const char* str) {
-		c_str_ = str;
+	MString(const char* str)
+	{
+		size_ = strlen(str);
+		// 필요한 길이만큼 문자열 동적할당
+		c_str_ = new char[size_ + 1];	// + 1 : null 공간
+		strcpy(c_str_, str);
 	}
 
 	// getter
 	unsigned int size() { return size_; }
-	const char* c_str() { return c_str_; }
+	char* c_str() { return c_str_; }
 
 private:
 	unsigned int size_;		// 문자열의 길이
-	const char* c_str_;			// 문자열을 가리키는 주소
+	char* c_str_;			// 문자열을 가리키는 주소
 };
 
 int main(void)
