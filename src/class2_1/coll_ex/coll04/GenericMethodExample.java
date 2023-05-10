017c4181561e97dbe8b90a9ee7f896d0db983e28
MsEmily1020 <peace10200khs@gmail.com>
2023-05-10 13:01:30 +0900

    [JAVA] GenericMethod

--- src/class2_1/coll_ex/coll04/GenericMethodExample.java
+++ src/class2_1/coll_ex/coll04/GenericMethodExample.java
@@ -1,11 +1,14 @@
 package class2_1.coll_ex.coll04;
 
 public class GenericMethodExample {
-	public static <T> GStack<T> reverse(GStack<T> a) {
-		GStack<T> s = new GStack<T>();
+	// <T> : 특정메소드에 제네릭 적용하겠다
+	// GStack<T> : reverse() 리턴 타입
+	// reverse(GStack<T> a) : a는 GStack클래스 객체변수
+	public static <T> GStack<T> reverse(GStack<T> a) {	// a = gs객체
+		GStack<T> s = new GStack<T>(); // GStack클래스 새로운 객체 생성
 		while(true) {
 			T tmp;
-			tmp = a .pop();
+			tmp = a .pop();	// 객체변수 a에 저장되어 있는 데이터 하나씩 읽어 tmp에 저장
 			if(tmp == null) break;
 			else s.push(tmp);
 		}
