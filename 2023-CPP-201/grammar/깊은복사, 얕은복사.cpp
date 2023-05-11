932d54795377a2680c699cabd1f11e18bfe8203b
MsEmily1020 <peace10200khs@gmail.com>
2023-05-11 10:08:01 +0900

    [C++] 깊은 복사

--- 2023-CPP-201/grammar/깊은복사, 얕은복사.cpp
+++ 2023-CPP-201/grammar/깊은복사, 얕은복사.cpp
@@ -2,21 +2,29 @@
 
 using namespace std;
 
-int main(void) 
+int main(void)
 {
 	// ptr1은 int 3개의 배열을 동적할당 시킨 후 가리킨다
 	int* ptr1 = new int[3];
 	ptr1[0] = 0, ptr1[1] = 10, ptr1[2] = 20;
 
-	// 얕은복사(shallow copy) : ptr2는 별도의 동적할당을 하지 않고 ptr1의 주소값만 복사함
-	int* ptr2 = ptr1;
+	// 깊은 복사(deep copy) : ptr2는 별도의 공간을 동적할당 후 ptr1의 값 복사
+	int* ptr2 = new int[3];
+	for (int i = 0; i < 3; i++)
+	{
+		ptr2[i] = ptr1[i];
+	}
 
-	// 얕은 복사를 하면 원본 데이터에 종속된다.
-	// 즉, ptr2[1] = 100으로 바뀜(고유의 데이터x)
+	// 깊은 복사를 하면 고유의 공간을 가지므로 ptr2[1]은 100으로 바뀌지 않음
 	ptr1[1] = 100;
 	for (int i = 0; i < 3; i++) 
 	{
 		cout << ptr1[i] << " " << ptr2[i] << endl;
 	}
+
+	// 얕은 복사 : delete[] ptr1만 해도 ptr2의 데이터 같이 없어지는 것
+	// 깊은 복사 : 둘의 공간이 다르므로 따로 따로 삭제 가능
+	delete[] ptr1;
+	delete[] ptr2;
 	return 0;
 }
