272335876c36b982b09e12ecaedc5262b6e07eb3
MsEmily1020 <peace10200khs@gmail.com>
2023-05-16 09:13:07 +0900

    [C++] 복사생성자의 얕은복사로 문제점 발생

--- 2023-CPP-201/grammar/복사생성자.cpp
+++ 2023-CPP-201/grammar/복사생성자.cpp
@@ -14,6 +14,12 @@ public :
 		cout << "일반생성자 호출" << endl;
 	}
 
+	// 복사 생성자(얕은 복사로 인하여 문제점 발생)
+	Character(const Character& m) : str_(m.str_)
+	{
+		cout << "복사생성자 호출" << endl;
+	}
+
 	// 소멸자
 	~Character()
 	{
@@ -26,5 +32,6 @@ private :
 
 int main(void)
 {
-	Character ch1 = Character("abc");
+	Character ch1 = Character("abc");	// 일반생성자 호출
+	Character ch2 = ch1;				// 복사생성자 호출
 }
