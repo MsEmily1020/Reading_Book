d5edde72937f67ee6120409dde38a7af339760bb
MsEmily1020 <peace10200khs@gmail.com>
2023-05-23 09:49:11 +0900

    [C++] SFML 뱀 사과 규격 맞추기

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -12,14 +12,16 @@ int main() {
 
 	srand(time(NULL));
 
+	int block = 40;
+
 	RectangleShape snake;
 	snake.setPosition(200, 300);
-	snake.setSize(Vector2f(30, 30));
+	snake.setSize(Vector2f(block, block));
 	snake.setFillColor(Color::Green);
 
 	RectangleShape apple;
-	apple.setPosition(rand() % 640 - 30, rand() % 480 - 30);
-	apple.setSize(Vector2f(30, 30));
+	apple.setPosition(rand() % 640 - block, rand() % 480 - block);
+	apple.setSize(Vector2f(block, block));
 	apple.setFillColor(Color::Red);
 
 	while (window.isOpen())
@@ -49,7 +51,7 @@ int main() {
 		// 뱀이 사과를 먹었을 때
 		if (snake.getGlobalBounds().intersects(apple.getGlobalBounds()))
 		{
-			apple.setPosition(rand() % 640 - 30, rand() % 480 - 30);
+			apple.setPosition(rand() % 640 - block, rand() % 480 - block);
 		}
 
 		// render
