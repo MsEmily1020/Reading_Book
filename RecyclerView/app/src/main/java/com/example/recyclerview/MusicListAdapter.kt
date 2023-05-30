4f12ad7ec4ea20dd274efaf883249f386501dcc8
MsEmily1020 <peace10200khs@gmail.com>
2023-05-31 07:35:28 +0900

    [Android] RecyclerView Test theme : Music

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MusicListAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MusicListAdapter(val data: List<Music>) : RecyclerView.Adapter<MusicListAdapter.ItemViewHolder>() {
+    class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(viewType, parent, false)
+        // viewType : Layout id
+        // parent : ViewGroup
+        return ItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = data[position]
+        val nameTextView = holder.view.findViewById<TextView>(R.id.music_name)
+        val genreTextView = holder.view.findViewById<TextView>(R.id.music_genre)
+        val imgImageView = holder.view.findViewById<ImageView>(R.id.music_img)
+        nameTextView.text = item.name
+        genreTextView.text = item.genre
+        imgImageView.setImageResource(item.img)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return R.layout.music_item
+    }
+}
