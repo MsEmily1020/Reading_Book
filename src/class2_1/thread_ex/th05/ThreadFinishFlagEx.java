352ecdb68ef673de94eb8c10f5b5d9cc0c2b1fd4
MsEmily1020 <peace10200khs@gmail.com>
2023-06-21 15:55:41 +0900

    [JAVA] RandomThread

---
+++ src/class2_1/thread_ex/th05/ThreadFinishFlagEx.java
@@ -0,0 +1,37 @@
+package class2_1.thread_ex.th05;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+import javax.swing.TransferHandler;
+
+public class ThreadFinishFlagEx extends JFrame {
+	private RandomThread th;
+	
+	public ThreadFinishFlagEx() {
+		setTitle("ThreadFinishFlag Ex 예제");
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		c.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				th.finish();
+			}
+		});
+		
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setSize(300, 200);
+		
+		th = new RandomThread(c);
+		th.start();
+	}
+	public static void main(String[] args) {
+		new ThreadFinishFlagEx();
+	}
+}
