14e269820aba6ad093b0ff7de92861db0e7a4f78
MsEmily1020 <peace10200khs@gmail.com>
2023-07-09 16:27:49 +0900

    [스프링 부트3 백엔드 개발자 되기] PUT 요청이 왔을 때 request 정보 받은 후 응답 값 전송하는 updateArticle 메서드 작성

--- Spring/스프링부트 3 백엔드 개발자 되기/Code/src/main/java/org/emily/springbootdeveloper/controller/BlogApiController.java
+++ Spring/스프링부트 3 백엔드 개발자 되기/Code/src/main/java/org/emily/springbootdeveloper/controller/BlogApiController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.emily.springbootdeveloper.domain.Article;
 import org.emily.springbootdeveloper.dto.AddArticleRequest;
 import org.emily.springbootdeveloper.dto.ArticleResponse;
+import org.emily.springbootdeveloper.dto.UpdateArticleRequest;
 import org.emily.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -49,5 +50,15 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+                                                 @RequestBody UpdateArticleRequest request) {
+
+        Article updatedArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
 }
