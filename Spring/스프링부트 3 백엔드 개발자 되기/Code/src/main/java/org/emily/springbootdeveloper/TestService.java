8c91339db94b5f27ced98f6ff8943c75221002ca
MsEmily1020 <peace10200khs@gmail.com>
2023-07-09 14:51:12 +0900

    [스프링 부트3 개발자 되기] TestService 비즈니스 계층 코드 작성

--- Spring/스프링부트 3 백엔드 개발자 되기/Code/src/main/java/org/emily/springbootdeveloper/TestService.java
+++ Spring/스프링부트 3 백엔드 개발자 되기/Code/src/main/java/org/emily/springbootdeveloper/TestService.java
@@ -1,2 +1,17 @@
-package org.emily.springbootdeveloper;public class TestService {
+package org.emily.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Member;
+import java.util.List;
+
+@Service
+public class TestService {
+    @Autowired
+    MemberRepository memberRepository;
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
 }
