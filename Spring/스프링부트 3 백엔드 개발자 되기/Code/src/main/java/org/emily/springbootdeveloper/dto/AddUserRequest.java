f31933dfdcf4c4085af10cc28fadc208f6c4fb52
MsEmily1020 <peace10200khs@gmail.com>
2023-07-10 11:19:49 +0900

    [스프링 부트3 백엔드 개발자 되기] 사용자 정보를 담고 있는 객체 AddUserRequest 생성

---
+++ Spring/스프링부트 3 백엔드 개발자 되기/Code/src/main/java/org/emily/springbootdeveloper/dto/AddUserRequest.java
@@ -0,0 +1,11 @@
+package org.emily.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
