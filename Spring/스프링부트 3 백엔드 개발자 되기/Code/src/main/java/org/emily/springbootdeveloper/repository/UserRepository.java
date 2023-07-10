ca632de610d69b8857d2af67470b19e4826da7b7
MsEmily1020 <peace10200khs@gmail.com>
2023-07-10 11:08:31 +0900

    [스프링 부트3 백엔드 개발자 되기] UserRepository 생성

---
+++ Spring/스프링부트 3 백엔드 개발자 되기/Code/src/main/java/org/emily/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.emily.springbootdeveloper.repository;
+
+import org.emily.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
