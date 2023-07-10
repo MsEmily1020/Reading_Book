cef7df6fa9a6fd83021433bdd4ea080111aa4132
MsEmily1020 <peace10200khs@gmail.com>
2023-07-10 09:46:48 +0900

    [스프링 부트3 백엔드 개발자 되기] 수정 화면을 보여주기 위한 컨트롤러 newArticle 메서드 작성

--- Spring/스프링부트 3 백엔드 개발자 되기/Code/src/main/java/org/emily/springbootdeveloper/controller/BlogViewController.java
+++ Spring/스프링부트 3 백엔드 개발자 되기/Code/src/main/java/org/emily/springbootdeveloper/controller/BlogViewController.java
@@ -1,6 +1,7 @@
 package org.emily.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.emily.springbootdeveloper.domain.Article;
 import org.emily.springbootdeveloper.dto.ArticleListViewResponse;
 import org.emily.springbootdeveloper.dto.ArticleViewResponse;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -35,5 +37,19 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        }
+
+        else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "new Article";
     }
 }
