5f7ce31678b546f088f884b0824e88c037f39e9b
MsEmily1020 <peace10200khs@gmail.com>
2023-07-10 09:20:22 +0900

    [스프링 부트3 백엔드 개발자 되기] id 블로그 글 article 반환하는 view controller getArticle() 메서드 작성

--- Spring/스프링부트 3 백엔드 개발자 되기/Code/src/main/java/org/emily/springbootdeveloper/controller/BlogViewController.java
+++ Spring/스프링부트 3 백엔드 개발자 되기/Code/src/main/java/org/emily/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package org.emily.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.emily.springbootdeveloper.domain.Article;
 import org.emily.springbootdeveloper.dto.ArticleListViewResponse;
+import org.emily.springbootdeveloper.dto.ArticleViewResponse;
 import org.emily.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,5 +27,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
