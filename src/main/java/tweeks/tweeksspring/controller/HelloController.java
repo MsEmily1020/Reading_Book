b0bf5af3417116d2e094f5b4d43d90a97c5635b1
MsEmily1020 <peace10200khs@gmail.com>
2023-07-30 22:40:35 +0900

    ✨ ResponseBody와 template의 차이점

--- src/main/java/tweeks/tweeksspring/controller/HelloController.java
+++ src/main/java/tweeks/tweeksspring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,5 +21,12 @@ public class HelloController {
         // key : name, value : 파라미터 name 값
         model.addAttribute("name", name);
         return "hello-template"; // template : hello-template 실행
+    }
+
+    @GetMapping("hello-spring")
+    @ResponseBody // http body에 return 값 직접 넣어줌
+    // template 차이점 : 페이지 소스 보기를 할 경우 해당 return 문자만 보여짐
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
     }
 }
