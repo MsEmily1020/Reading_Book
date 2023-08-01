af02207dc1563ddf68c5c41a40418d2415947f41
MsEmily1020 <peace10200khs@gmail.com>
2023-08-01 12:35:20 +0900

    💄 [2강] Icon 떨어트려 가운데 정렬

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Icon(Icons.star),
             Icon(Icons.star),
