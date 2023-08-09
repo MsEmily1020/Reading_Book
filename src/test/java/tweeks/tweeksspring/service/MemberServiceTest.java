d944359a753b9ab72edeb0daf6f695a1ab23eb75
MsEmily1020 <peace10200khs@gmail.com>
2023-08-09 22:34:41 +0900

    ✨ MemberService beforeEach, Constructor 생성

--- src/test/java/tweeks/tweeksspring/service/MemberServiceTest.java
+++ src/test/java/tweeks/tweeksspring/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package tweeks.tweeksspring.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tweeks.tweeksspring.domain.Member;
 import tweeks.tweeksspring.repository.MemoryMemberRepository;
@@ -11,8 +12,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
 
     @AfterEach
     public void afterEach() {
