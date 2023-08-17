4e5c2710d9b65dc165d659134bbc2becc689b95d
MsEmily1020 <peace10200khs@gmail.com>
2023-08-18 07:53:04 +0900

    (1.13) 배열에 항목 추가하기
    등록버튼을 눌렀을 때 input값 잘 초기화 되는지 확인

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { useRef } from "react";
-import UserList from "./UserList";
-import CreateUser from "./CreateUser";
+import React, { useRef, useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: ''
+    });
+    const { username, email } = inputs;
+    const onChange = e => {
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
     const users = [
         {
             id: 1,
             username: 'velopert',
             email: 'public.velopert@gmail.com'
         },
-
         {
             id: 2,
             username: 'tester',
             email: 'tester@example.com'
         },
-
         {
             id: 3,
             username: 'liz',
@@ -25,13 +35,24 @@ function App() {
 
     const nextId = useRef(4);
     const onCreate = () => {
-      nextId.current += 1;
-    };
+        // 나중에 구현 할 배열에 항목 추가하는 로직
+        // ...
 
+        setInputs({
+            username: '',
+            email: ''
+        });
+        nextId.current += 1;
+    };
     return (
         <>
-            <CreateUser/>
-            <UserList users={users}/>
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+            <UserList users={users} />
         </>
     );
 }
