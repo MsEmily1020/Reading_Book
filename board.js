571bd52af7c01351560628ff6d5c364092013801
MsEmily1020 <peace10200khs@gmail.com>
2023-10-27 16:18:43 +0900

    ✨ 게시글 crd 작업 완성

---
+++ board.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+
+const app = express();
+app.use(bodyParser.json());
+
+const port = 3000;
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "hozu",
+});
+
+app.post("/posts", (req, res) => {
+  pool.query(
+    "insert into post(title, author, createdAt, content) values (?, ?, NOW(), ?)",
+    [req.body.title, req.body.author, req.body.content],
+    (err, rows, fields) => {
+      if (err) res.status(400).json({ result: err });
+      else res.json({ result: "ok" });
+    }
+  );
+});
+
+app.get("/posts", (req, res) => {
+  pool.query("select * from post", (err, rows, fields) => {
+    res.json({ result: rows });
+  });
+});
+
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  pool.query("select * from post where id = ?", [id], (err, rows, fields) => {
+    if (rows.length == 0) res.status(404).send({ result: null });
+    else res.json({ result: rows[0] });
+  });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  pool.query("delete from post where id = ?", [id], (err, rows, fields) => {
+    if (rows.affectedRows === 0) res.status(404).json({ result: null });
+    else res.json({ result: "ok" });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
