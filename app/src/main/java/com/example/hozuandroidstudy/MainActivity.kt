5211567dede85f40e551f58782f07a740e4bbc70
MsEmily1020 <peace10200khs@gmail.com>
2023-11-02 18:51:13 +0900

    ✨ api retrofit create, delete 기능 구현

--- app/src/main/java/com/example/hozuandroidstudy/MainActivity.kt
+++ app/src/main/java/com/example/hozuandroidstudy/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import com.example.hozuandroidstudy.api.APIService
 import com.example.hozuandroidstudy.api.AllPostResponse
+import com.example.hozuandroidstudy.api.PostCreateRequest
 import com.example.hozuandroidstudy.api.PostResponse
+import com.example.hozuandroidstudy.api.StringResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -25,7 +27,10 @@ class MainActivity : AppCompatActivity() {
 
         apiService = retrofit.create(APIService::class.java)
         // getPosts()
-        getPost(2)
+        // getPost(2)
+        val post = PostCreateRequest("title1", "author1", "content1")
+        createPost(post)
+        deletePost(2)
     }
 
     fun getPosts() {
@@ -59,6 +64,36 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<PostResponse>, t: Throwable) {}
+        })
+    }
+
+    fun createPost(post: PostCreateRequest) {
+        val call = apiService.createPost(post)
+        call.enqueue(object: Callback<StringResponse> {
+            override fun onResponse(call: Call<StringResponse>, response: Response<StringResponse>) {
+                val data: StringResponse? = response.body()
+                data?.let {
+                    Log.d("mytag", it.result.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<StringResponse>, t: Throwable) {}
+
+        })
+    }
+
+    fun deletePost(id: Int) {
+        val call = apiService.deletePost(id)
+        call.enqueue(object: Callback<StringResponse> {
+            override fun onResponse(call: Call<StringResponse>, response: Response<StringResponse>) {
+                val data: StringResponse? = response.body()
+                data?.let {
+                    Log.d("mytag", it.result.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<StringResponse>, t: Throwable) {}
+
         })
     }
 }
--- app/src/main/java/com/example/hozuandroidstudy/api/APIService.kt
+++ app/src/main/java/com/example/hozuandroidstudy/api/APIService.kt
@@ -1,13 +1,18 @@
 package com.example.hozuandroidstudy.api
 
 import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 import java.util.Date
 
 data class Post(val id: Int, var title: String, var author: String, var createdAt: Date, var content: String)
 data class AllPostResponse(val result: List<Post>)
 data class PostResponse(val result: Post)
+data class PostCreateRequest(val title: String, val author: String, val content: String)
+data class StringResponse(val result: String)
 
 interface APIService {
     @GET("/posts")
@@ -15,4 +20,10 @@ interface APIService {
 
     @GET("/posts/{id}")
     fun getPost(@Path("id") id: Int) : Call<PostResponse>
+
+    @POST("/posts")
+    fun createPost(@Body request: PostCreateRequest) : Call<StringResponse>
+
+    @DELETE("/posts/{id}")
+    fun deletePost(@Path("id") id: Int) : Call<StringResponse>
 }
