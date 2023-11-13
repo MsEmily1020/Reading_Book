cce388f1da0a1927d13e6da7fafa4d989b0643d5
MsEmily1020 <peace10200khs@gmail.com>
2023-11-14 08:52:30 +0900

    [C++] static 멤버변수

---
+++ 2023-CPP-201/grammar/static.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Champ {
+public:
+	// 함수의 선언
+	Champ(const string& name);
+private:
+	static int cnt_;
+};
+
+// 함수를 바깥에서 정의할 때는 멤버함수나 멤버변수 이름 앞에 클리스 이름::
+Champ::Champ(const string& name) {
+	++cnt_;
+}
+
+int Champ::cnt_ = 0;
+
+int main(void)
+{
+	Champ* a = new Champ("hello");
+	Champ* b = new Champ("world");
+	// Champ::cnt_의 값은 2
+	// cnt_변수는 모두가 공유하기 때문에 1개만 있음
+	// Champ형 객체는 2개 있음
+}
