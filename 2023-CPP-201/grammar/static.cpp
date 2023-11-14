b4fcc6d71954e78d21585959797e09a2bd5d5c24
MsEmily1020 <peace10200khs@gmail.com>
2023-11-14 09:42:23 +0900

    [C++] static 멤버함수

--- 2023-CPP-201/grammar/static.cpp
+++ 2023-CPP-201/grammar/static.cpp
@@ -7,6 +7,10 @@ class Champ {
 public:
 	// 함수의 선언
 	Champ(const string& name);
+	static int get_cnt() {
+		return cnt_;
+	}
+
 private:
 	static int cnt_;
 };
@@ -16,6 +20,7 @@ Champ::Champ(const string& name) {
 	++cnt_;
 }
 
+// static 멤버변수는 무조건 바깥에서 정의하기 (const 제외)
 int Champ::cnt_ = 0;
 
 int main(void)
@@ -25,4 +30,10 @@ int main(void)
 	// Champ::cnt_의 값은 2
 	// cnt_변수는 모두가 공유하기 때문에 1개만 있음
 	// Champ형 객체는 2개 있음
+
+	// 객체를 생성하지 않아도 static 멤버함수를 호출 할 수 있음
+	cout << Champ::get_cnt() << endl;
+	cout << a->get_cnt() << endl;
+	cout << b->get_cnt() << endl;
+
 }
